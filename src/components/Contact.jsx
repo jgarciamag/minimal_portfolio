e7def358fb8c2933310feda6ab2580374c93c6f2
jgarciamag <166933905+jgarciamag@users.noreply.github.com>
2024-04-15 22:41:48 -0600

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,15 +6,15 @@ function Contact() {
       <div className="flex flex-col mb-10 mx-auto">
          <div className="flex justify-center items-center">
             <form
-               action="https://getform.io/f/(customSlugHere)"
+               action="https://getform.io/f/qbloqewa"
                method="POST"
                className="flex flex-col w-full md:w-7/12"
             >
-               <Title>Contact</Title>
+               <Title>Contacto</Title>
                <input
                   type="text"
                   name="name"
-                  placeholder="Name"
+                  placeholder="Nombre"
                   className="p-2 bg-transparent border-2 rounded-md focus:outline-none"
                />
                <input
@@ -25,15 +25,15 @@ function Contact() {
                />
                <textarea
                   name="message"
-                  placeholder="Message"
+                  placeholder="Mensaje"
                   rows="10"
                   className="p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none"
                />
                <button
-                  type="button"
+                  type="submit"
                   className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-yellow-500 to-pink-500 drop-shadow-md hover:stroke-white"
                >
-                  Work With Me
+                  ¡Mándame un mensaje!
                </button>
             </form>
          </div>
