e605552c2a5cde9b82ed976f4fd335b2a4776fc1
jgarciamag <166933905+jgarciamag@users.noreply.github.com>
2024-04-15 22:52:56 -0600

    Update portfolio.js

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,20 +1,60 @@
 export default [
    {
-      title: 'Terminal Website',
-      imgUrl: '/assets/terminal-website.jpg',
-      stack: ['HTML', 'CSS', 'JavaScript'],
-      link: 'https://fkcodes.com',
+      title: 'Bloomberg Scraper (Noticias)',
+      imgUrl: '/assets/bloomberg.png',
+      stack: [' Automatiza la extracción de noticias de Bloomberg. Utilizando técnicas de web scraping, el código recopila artículos recientes con ciertos parámetros y los envía automáticamente desde un servidor en la nube a los usuarios en un horario preestablecido.'],
+      link: 'https://youtu.be/in5pXcN-5bU',
    },
    {
-      title: 'Developer Portfolio',
-      imgUrl: '/assets/dev-portfolio.jpg',
-      stack: ['React', 'Tailwind', 'Vite'],
-      link: 'https://thehackershideout.com',
+      title: 'Portfolio Optimizator',
+      imgUrl: '/assets/financial_portfolio.jpg',
+      stack: ['Optimiza la gestión de portafolios de inversión. Calcula el rendimiento diario, mensual y YTD de un portafolio compuesto por diversos activos financieros. Analiza las fluctuaciones diarias en el valor de cada activo dentro del portafolio, permitiendo monitorear estrategias de inversión y ajustarlas conforme a los resultados obtenidos.'],
+      link: 'https://youtu.be/NL-K_t8VJvs',
    },
    {
-      title: 'AI Nerf Gun',
-      imgUrl: '/assets/ai-nerf-gun.jpg',
-      stack: ['Python', 'C++', 'Robotics'],
-      link: 'https://youtu.be/A8eegxL82PI',
+      title: 'VaR Portafolios Optimizator',
+      imgUrl: '/assets/VaR.png',
+      stack: ['Optimiza el cálculo del VaR a cierto nivel de confianza de un portafolio usando simulaciones. Se usan Rendimientos aleatorios estandarizados independientes y Rendimientos no estandarizados correlacionados y el modelo Garch de J.P.Morgan. para la volatilidad prospectiva. Ayuda a cuantificar el nivel de riesgo asociado a sus estrategias de inversión, permitiendo tomar decisiones más informadas. '],
+      link: 'https://youtu.be/X4dySmzs7Wg',
    },
+
+   {
+      title: 'Automator de Facturas',
+      imgUrl: '/assets/fenix.png',
+      stack: ['Simplifica y automatiza el proceso de gestión de factura utilizando técnicas  de llenado automático en sitios web para ingresar y procesar datos de facturación, reduciendo significativamente el tiempo y el esfuerzo requeridos para manejar grandes volúmenes de facturas. Acelera la entrada de datos y minimiza los errores humanos.'],
+      link: 'https://youtu.be/vYuvdhTJgfM',
+   },
+   {
+      title: 'J.P.Morgan Scraper',
+      imgUrl: '/assets/jpmorgan.jpg',
+      stack: ['Automatiza la extracción del "Weekly Market Recap" de J.P. Morgan, un resumen semanal clave que ofrece insights y análisis sobre las tendencias del mercado financiero estadounidense y lo envía automáticamente desde un servidor en la nube a los usuarios en un horario preestablecido.'],
+      link: 'https://youtu.be/in5pXcN-5bU',
+   },
+
+   {
+      title: 'Valmex Scraper',
+      imgUrl: '/assets/valmex.png',
+      stack: ['Automatiza la extracción del Reporte Diario y Semanal que Valmex ofrece. Estos resúmenes proporcionan una visión crucial sobre las métricas principales del mercado, ayudando a los usuarios a entender mejor los movimientos y oportunidades económicas. El programa recopila esta información y la envía automáticamente desde un servidor en la nube a los usuarios en horarios preestablecidos.'],
+      link: 'https://youtu.be/in5pXcN-5bU',
+   },
+   {
+      title: 'Auctions Scraper',
+      imgUrl: '/assets/lagazette.jpg',
+      stack: ['Automatiza la extracción de información de una página web de subastas,facilitando la recopilación de datos de los artículos en subasta, como precios de inicio, pujas actuales, tiempos de cierre, vendedor e información del activo subyacente. Es una solución práctica y eficaz para seguir de cerca y capitalizar las subastas en línea.'],
+      link: 'https://youtu.be/Tqd585T3NqQ',
+   },
+   {
+      title: 'Python for Algorithmic Trading',
+      imgUrl: '/assets/trading.jpg',
+      stack: ['En proceso de meter Machine Learning (Logit & Probit Regression Model) a un portafolio de inversión.'],
+      link: 'https://youtu.be/YB6_fLMEQCo',
+   },
+   {
+      title: 'Heat Map (Finviz) + Whatsapp',
+      imgUrl: '/assets/finviz.jpeg',
+      stack: ['En proceso de meter crear un código que entre a FinViz, una pagina que te da el heat map del mercado actual, y mande a un grupo de Whatsapp de Finanzas el heat map del mercado a la hora de apertura de los mercados y a la hora de cierre con el fin de tener un panaroma completo de las acciones que subieron y bajaron en el día.'],
+      link: 'https://youtu.be/U0gKv_aHDWc',
+   },
+
+
 ]
