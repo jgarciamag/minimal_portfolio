8e2b7295099a80b5c1cd05f0ca34c00b2efd5322
jgarciamag <166933905+jgarciamag@users.noreply.github.com>
2024-04-30 09:03:08 -0600

    Update portfolio.js

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -21,7 +21,7 @@ export default [
    {
       title: 'Automator de Facturas',
       imgUrl: '/assets/fenix.png',
-      stack: ['Simplifica y automatiza el proceso de gestión de factura utilizando técnicas  de llenado automático en sitios web para ingresar y procesar datos de facturación, reduciendo significativamente el tiempo y el esfuerzo requeridos para manejar grandes volúmenes de facturas. Acelera la entrada de datos y minimiza los errores humanos.'],
+      stack: ['Simplifica y automatiza el proceso de gestión de factura utilizando técnicas  de llenado automático en sitios web para ingresar y procesar datos de facturación, reduciendo significativamente el tiempo y el esfuerzo requeridos para manejar grandes volúmenes de facturas. Este código ayuda a acelerar la entrada de datos y minimiza los errores humanos.'],
       link: 'https://youtu.be/vYuvdhTJgfM',
    },
    {
