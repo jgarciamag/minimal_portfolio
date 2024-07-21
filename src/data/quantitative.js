7472ed4aae390425da0fd2625d0eb6717a61f64f
jgarciamag <166933905+jgarciamag@users.noreply.github.com>
2024-07-21 16:16:07 -0600

    Update quantitative.js

--- src/data/quantitative.js
+++ src/data/quantitative.js
@@ -1,44 +1,45 @@
-export default [
-   {
-      title: 'How They Make Money | App Economy Insights',
-      imgUrl: '/assets/sankey.jpg',
-      stack: ['Simplifica y automatiza el proceso de recopilación de información contable tanto de los formularios 10-K como 10-Q obtenidos de la página oficial de la SEC de cualquier empresa que cotiza en la bolsa estadounidense. Además, genera gráficos tipo Sankey similares a los de appeconomyinsights.com, facilitando el análisis visual de los flujos financieros dentro de la empresa.'],
-      link: 'https://youtu.be/thGqbvXLIKs',
-   },
+// export default [
+//    {
+//       title: 'How They Make Money | App Economy Insights',
+//       imgUrl: '/assets/sankey.jpg',
+//       stack: ['Simplifica y automatiza el proceso de recopilación de información contable tanto de los formularios 10-K como 10-Q obtenidos de la página oficial de la SEC de cualquier empresa que cotiza en la bolsa estadounidense. Además, genera gráficos tipo Sankey similares a los de appeconomyinsights.com, facilitando el análisis visual de los flujos financieros dentro de la empresa.'],
+//       link: 'https://youtu.be/thGqbvXLIKs',
+//    },
 
-   {
-      title: 'Tactical Sector & Industry Framework of S&P500 (Quantitative Sector)',
-      imgUrl: '/assets/BofA.jpg',
-      stack: ['Este código replica los resultados del documento "S&P 500 Relative Value Cheat Sheet" de Bank of America, el cual explora el uso de enfoques cuantitativos para evaluar el valor relativo de los sectores del S&P 500. En particular, el estudio clasifica los sectores en términos de Opportunities, Value Traps y Momentum Breakdowns. Se implemento para los sectores del S&P500 al igual que para diferntes países.'],
-      link: 'https://drive.google.com/file/d/1Huw1oXRuoW9rauS3I0HXeLCEFEyQSCRO/view?usp=sharing',
-   },
+//    {
+//       title: 'Tactical Sector & Industry Framework of S&P500 (Quantitative Sector)',
+//       imgUrl: '/assets/BofA.jpg',
+//       stack: ['Este código replica los resultados del documento "S&P 500 Relative Value Cheat Sheet" de Bank of America, el cual explora el uso de enfoques cuantitativos para evaluar el valor relativo de los sectores del S&P 500. En particular, el estudio clasifica los sectores en términos de Opportunities, Value Traps y Momentum Breakdowns. Se implemento para los sectores del S&P500 al igual que para diferntes países.'],
+//       link: 'https://drive.google.com/file/d/1Huw1oXRuoW9rauS3I0HXeLCEFEyQSCRO/view?usp=sharing',
+//    },
 
-   {
-      title: 'Market Sentiment Analysis (NLP)',
-      imgUrl: '/assets/sentiment.png',
-      stack: ['Este código permite realizar análisis de sentimiento, una técnica que extrae información y sentimientos de textos, tales como artículos de noticias, publicaciones en redes sociales y reportes financieros. En el ámbito financiero, el análisis de sentimiento interpreta la opinión pública para evaluar el sentimiento del mercado, proporcionando información valiosa para traders e inversionistas. Se uso Natural Language Processing y VADER.'],
-      link: 'https://drive.google.com/file/d/11ed8Corp-rYxiq57kyHfNd6pQwbPBl-x/view?usp=sharing',
-   },
+//    {
+//       title: 'Market Sentiment Analysis (NLP)',
+//       imgUrl: '/assets/sentiment.png',
+//       stack: ['Este código permite realizar análisis de sentimiento, una técnica que extrae información y sentimientos de textos, tales como artículos de noticias, publicaciones en redes sociales y reportes financieros. En el ámbito financiero, el análisis de sentimiento interpreta la opinión pública para evaluar el sentimiento del mercado, proporcionando información valiosa para traders e inversionistas. Se uso Natural Language Processing y VADER.'],
+//       link: 'https://drive.google.com/file/d/11ed8Corp-rYxiq57kyHfNd6pQwbPBl-x/view?usp=sharing',
+//    },
 
-   {
-      title: 'Modelo de Flujos',
-      imgUrl: '/assets/flujos.png',
-      stack: ['Este código permite realizar análisis de flujos.'],
-      link: 'https://drive.google.com/file/d/1Hcnh-J-xmpePT4ZAZZomyFQjr7G_kOg7/view?usp=sharing',
-   },
+//    {
+//       title: 'Modelo de Flujos',
+//       imgUrl: '/assets/flujos.png',
+//       stack: ['Este código permite realizar análisis de flujos.'],
+//       link: 'https://drive.google.com/file/d/1Hcnh-J-xmpePT4ZAZZomyFQjr7G_kOg7/view?usp=sharing',
+//    },
 
-   {
-      title: 'Modelo Autoregresivo',
-      imgUrl: '/assets/media_movil.png',
-      stack: ['Este código permite realizar un modelo autorregresivo de media movil para realizar un forecast.'],
-      link: 'https://drive.google.com/file/d/1-vmtHGJ8CfXQ2M3RrTQTzRNrg6AEuHlc/view?usp=sharing',
-   },
+//    {
+//       title: 'Modelo Autoregresivo',
+//       imgUrl: '/assets/media_movil.png',
+//       stack: ['Este código permite realizar un modelo autorregresivo de media movil para realizar un forecast.'],
+//       link: 'https://drive.google.com/file/d/1-vmtHGJ8CfXQ2M3RrTQTzRNrg6AEuHlc/view?usp=sharing',
+//    },
 
-   {
-      title: 'Modelo de Métricas',
-      imgUrl: '/assets/sharpe.png',
-      stack: ['Este script de Python se ha diseñado para calcular diversas métricas financieras clave en una sola función, lo que facilita la evaluación del rendimiento y riesgo de portafolios de inversión. Las métricas calculadas incluyen el Sharpe Ratio, el Information Ratio, y el Max Drawdown. Este enfoque integral proporciona a los analistas e inversores una herramienta eficiente para obtener una visión completa de la performance de sus inversiones.'],
-      link: 'https://drive.google.com/file/d/1fvBj_asFyBuAX5heHQIb5PHsw2xrYMql/view?usp=sharing',
-   },
+//    {
+//       title: 'Modelo de Métricas',
+//       imgUrl: '/assets/sharpe.png',
+//       stack: ['Este script de Python se ha diseñado para calcular diversas métricas financieras clave en una sola función, lo que facilita la evaluación del rendimiento y riesgo de portafolios de inversión. Las métricas calculadas incluyen el Sharpe Ratio, el Information Ratio, y el Max Drawdown. Este enfoque integral proporciona a los analistas e inversores una herramienta eficiente para obtener una visión completa de la performance de sus inversiones.'],
+//       link: 'https://drive.google.com/file/d/1fvBj_asFyBuAX5heHQIb5PHsw2xrYMql/view?usp=sharing',
+//    },
 
-]
+// ]
+
